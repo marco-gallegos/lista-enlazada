f341fc70c15fdcc1b8e9618c92a72914f7f757ca
marco gallegos <ma_galeza@hotmail.com>
2016-11-10 17:27:23 -0600

    implementacion de la clase lista con sus prototipos

--- lista.h
+++ lista.h
@@ -9,9 +9,9 @@ template <class DATO> class Lista;
 template<class DATO> class Nodo
 {
 public:
+    //constructor sobrecargado no permite construir sin pasarle dat
     Nodo(DATO dat,Nodo<DATO> * ant=NULL, Nodo<DATO> * sig = NULL):valor(dat),anterior(ant),
-        siguiente(sig)
-    {    }
+        siguiente(sig) { }
 
     DATO valor;
     Nodo<DATO> *anterior,*siguiente;
@@ -19,6 +19,20 @@ public:
     friend class Lista<DATO>;
 
 };
+
+template<class DATO> class Lista
+{
+public:
+    Nodo<DATO> *head;
+
+    Lista():head(NULL){}
+
+    void insertar(DATO a_insertar);
+    void eliminar(DATO a_borrar);
+    //regresara el dato que busca
+    DATO buscar(dato &a_buscar);
+    void ultimo();
+    void mostrar();
 
 #endif // LISTA
 
